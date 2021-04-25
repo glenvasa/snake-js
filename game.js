880ca4597303ad8017887778fbbe4815f090009e
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-04-25 19:29:38 -0400

    completed all game functionality

--- game.js
+++ game.js
@@ -1,11 +1,22 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js'
-
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
+import { update as updateFood, draw as drawFood} from './food.js'
+import { outsideGrid} from './grid.js'
 
 // Game Loop
 
 let lastRenderTime = 0
+let gameOver = false
+const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
+    if(gameOver) {
+       if (confirm('You lost. Press ok to restart')){
+           window.location = '/'
+       }
+       return
+    }
+
+
     // tells us what the time is when we are able render the frame
     window.requestAnimationFrame(main)
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
@@ -21,15 +32,24 @@ function main(currentTime) {
     
 }
 
-// window.requestAnimationFrame(main)
+window.requestAnimationFrame(main)
 
 function update(){
     updateSnake()
+    updateFood()
+    checkDeath()
 }
 
 
 
 
 function draw(){
-    drawSnake()
+    // redraws board each time and removes old snake pieces
+    gameBoard.innerHTML = ''
+    drawSnake(gameBoard)
+    drawFood(gameBoard)
+}
+
+function checkDeath() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }

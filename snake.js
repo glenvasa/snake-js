9a7386f81c69565a805200ea14b259ef41c26a5e
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-04-25 17:37:46 -0400

    Create snake.js

---
+++ snake.js
@@ -0,0 +1,10 @@
+// how many times the snake moves per second
+export const SNAKE_SPEED = 2
+
+export function update() {
+    console.log('update snake');
+}
+
+export function draw(){
+    console.log('draw snake');
+}
